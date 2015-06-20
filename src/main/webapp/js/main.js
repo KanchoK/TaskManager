8a7d7c960c7c656d7abc48ec84597502918d8921
Kancho <kanchokanev7@gmail.com>
2015-06-20 17:15:35 +0300

    added functionality for hidding buttons

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -8,3 +8,30 @@ function logout() {
 				
 	});
 }
+
+function authorization() {
+	$.ajax({
+		url: 'rest/user/authorization',
+		type: 'GET',
+		success: function(data) {
+			hideButtons(data);
+		}			
+	});
+}
+
+function hideButtons(isAdmin) {
+	
+	if(isAdmin) {
+		$('#logoutButton').show();
+        $('#createTaskButton').show();
+        $('#createUserButton').show();
+        $('#editProfileButton').show();
+        $('#homeButton').show();
+	} else {
+		$('#logoutButton').show();
+        $('#createTaskButton').hide();
+        $('#createUserButton').hide();
+        $('#editProfileButton').show();
+        $('#homeButton').show();
+	}
+}
